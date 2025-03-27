bfa217c565e2dfc0a7c6f66e683df70412142d46
Sergio Salas <sss621@alumnos.unican.es>
2025-03-27 15:24:38 +0100

    Arreglado calculo.py, valor x faltaba n_surrounding
    Former-commit-id: df19748e4f8968fe2de73cda44dfba80c3a0fe51

--- mieWeb/src/negocio/calculo.py
+++ mieWeb/src/negocio/calculo.py
@@ -11,7 +11,7 @@ def calculate_mie_arrays(material_data, radius, n_surrounding):
 
     # Calculate the complex refractive index m and the size parameter x
     m = (n_array - 1.0j * k_array) / n_surrounding
-    x = 2 * np.pi * radius / lambda_array
+    x = 2 * np.pi * radius * n_surrounding/ lambda_array
 
     # Calculate the Mie scattering parameters using miepython
     qext, qsca, qback, g = miepython.mie(m, x)
